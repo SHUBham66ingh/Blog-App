2bbd23dde26777c09c9c07ecd4d6e4f6ab5b2931
Shubham singh <shubham1041singh@gmail.com>
2026-03-03 13:50:47 +0530

    inputAdded

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -7,7 +7,7 @@ import { useNavigate } from 'react-router-dom';
 const Header = () => {
      const authStatus = useSelector((state)=>state.auth.status) 
      const navigate = useNavigate()
-     const naItems = [
+     const navItems = [
           {
       name: 'Home',
       slug: "/",
@@ -36,7 +36,35 @@ const Header = () => {
      ]
 
   return (
-    <div>Header</div>
+      <header className='py-3 shadow bg-gray-500'>
+      <Container>
+        <nav className='flex'>
+          <div className='mr-4'>
+            <Link to='/'>
+              <Logo width='70px' />
+
+              </Link>
+          </div>
+          <ul className='flex ml-auto'>
+            {navItems.map((item) => 
+            item.active ? (
+              <li key={item.name}>
+                <button
+                onClick={() => navigate(item.slug)}
+                className='inline-bock px-6 py-2 duration-200 hover:bg-blue-100 rounded-full'
+                >{item.name}</button>
+              </li>
+            ) : null
+            )}
+            {authStatus && (
+              <li>
+                <LogoutBtn />
+              </li>
+            )}
+          </ul>
+        </nav>
+        </Container>
+    </header>
   )
 }
 
---
+++ src/components/input.jsx
@@ -0,0 +1,11 @@
+import React , {useId} from 'react'
+
+const Input({})
+ {
+    const id = useId()
+  return (
+    <div>input</div>
+  )
+}
+
+export default Input
