1a059f200fe93ea8fbfb689c08cd5fe80ffa8e62
Shubham singh <shubham1041singh@gmail.com>
2026-03-03 10:47:21 +0530

    header is updated

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,8 +1,42 @@
-import React from 'react'
+import React from 'react';
+import {Container , Logo , LogoutBtn} from '../index';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 
 const Header = () => {
+     const authStatus = useSelector((state)=>state.auth.status) 
+     const navigate = useNavigate()
+     const naItems = [
+          {
+      name: 'Home',
+      slug: "/",
+      active: true
+    }, 
+    {
+      name: "Login",
+      slug: "/login",
+      active: !authStatus,
+  },
+  {
+      name: "Signup",
+      slug: "/signup",
+      active: !authStatus,
+  },
+  {
+      name: "All Posts",
+      slug: "/all-posts",
+      active: authStatus,
+  },
+  {
+      name: "Add Post",
+      slug: "/add-post",
+      active: authStatus,
+  },
+     ]
+
   return (
-    <div>Footer</div>
+    <div>Header</div>
   )
 }
 
